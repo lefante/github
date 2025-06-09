8698fdd6f728086474d865c1c30f650b942f0481
lefante <leandrorodrigor09@gmail.com>
2025-06-09 14:28:10 -0300

    adiciona função divisão

--- projeto-branch/calculadora.py
+++ projeto-branch/calculadora.py
@@ -7,3 +7,13 @@ def multiplicar(a, b):
     return a * b
 
 print(multiplicar(2, 3))
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    return "Erro: divisão por zero"
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    return "Erro: divisão por zero"
